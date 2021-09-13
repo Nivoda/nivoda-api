b2cbdccb5139ac3884988b0628099f83cf320cda
Andre Woons <andre.woons@gmail.com>
2021-09-14 02:09:49 +0800

    Update nodejs.js

--- examples/nodejs.js
+++ examples/nodejs.js
@@ -54,7 +54,6 @@ let authenticate_query = `{
             video
             image
             availability
-            supplierStockId
             certificate {
               id
               shape
